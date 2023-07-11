43f7ca95a8f10d8a01e65ce7fcda757ffbdfa6be
wissemkarous <115191512+wissemkarous@users.noreply.github.com>
2023-07-11 13:49:47 +0100

    Add files via upload

--- TimesheetsScreen.js
+++ TimesheetsScreen.js
@@ -8,24 +8,31 @@ function TimesheetsScreen() {
   const [filteredUsers, setFilteredUsers] = useState([]);
 
   const data = [
-    { id: '1', name: 'karous wissem', dateString: '2023-07-11' },
-    { id: '2', name: 'karous wajih', dateString: '2023-07-11' },
-    { id: '3', name: 'ayoub chaari', dateString: '2023-07-12' },
-    { id: '4', name: 'jacer chetoui', dateString: '2023-07-12' },
-    { id: '5', name: 'leo messi', dateString: '2023-07-13' },
-    { id: '6', name: 'lotfi weld ama', dateString: '2023-07-13' },
-    { id: '7', name: 'mohamed ali', dateString: '2023-07-14' },
-    { id: '8', name: 'mohamed salah', dateString: '2023-07-14' },
-    { id: '9', name: 'mohamed amine', dateString: '2023-07-15' },
+    { id: '1', name: 'karous wissem', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '2', name: 'karous wajih', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '3', name: 'ayoub chaari', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '4', name: 'jacer chetoui', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '5', name: 'leo messi', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '6', name: 'lotfi weld ama', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '7', name: 'karouswissem', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '8', name: 'karouswajih', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '9', name: 'ayoubchaari', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '10', name: 'jacerchetoui', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '11', name: 'leomessi', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '12', name: 'lotfi weld ama', worktime:'from 8 am to 2 pm', dateString: '2023-07-11' },
+    { id: '13', name: 'mohamed ali', worktime:'from 8 am to 2 pm', dateString: '2023-07-14' },
+    { id: '14', name: 'mohamed salah', worktime:'from 8 am to 2 pm', dateString: '2023-07-14' },
+    { id: '15', name: 'mohamed amine', worktime:'from 8 am to 2 pm', dateString: '2023-07-15' },
     // Ajoutez les autres utilisateurs ici
   ];
 
   const renderItem = ({ item }) => (
     <View style={styles.userItem}>
-      <Text>Username: {item.name}</Text>
-      <Text>id: {item.id}</Text>
-    
-      {/* Ajoutez les éléments d'affichage de l'assiduité ici */}
+      <View>
+        <Text>Username: {item.name}</Text>
+        <Text>ID: {item.id}</Text>
+        <Text>Worktime: {item.worktime}</Text>
+      </View>
     </View>
   );
 
@@ -72,21 +79,16 @@ function TimesheetsScreen() {
           }}
         />
       </View>
-      <View style={styles.userListContainer}>
-        <FlatList
-          data={searchQuery && selectedDate ? filteredUsers : []}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-      <View style={styles.selectedUsersContainer}>
-        <Text style={styles.selectedUsersTitle}>Utilisateurs du jour sélectionné:</Text>
-        <FlatList
-          data={filteredUsers}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
+      {selectedDate && (
+        <View style={styles.selectedUsersContainer}>
+          <Text style={styles.selectedUsersTitle}>Users of the selected day:</Text>
+          <FlatList
+            data={filteredUsers}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -114,9 +116,14 @@ const styles = StyleSheet.create({
     width: '100%',
     marginBottom: 16,
   },
-  userListContainer: {
+  selectedUsersContainer: {
     flex: 1,
     width: '100%',
+  },
+  selectedUsersTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
   },
   userItem: {
     flexDirection: 'row',
@@ -129,14 +136,6 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#e0e0e0',
-  },
-  selectedUsersContainer: {
-    marginTop: 16,
-  },
-  selectedUsersTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 8,
   },
 });
 
